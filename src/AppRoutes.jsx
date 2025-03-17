80bac78430b55a783cbe293cdd7427961b91ae25
Metehan Olgun <metehannolgun@gmail.com>
2025-03-17 16:27:39 +0300

    -Verilerin tutulacağı data.js eklendi. -Tarif detayları için yeni bir page oluşturuldu. -Sağlıklı tarifler sayfası düzenlendi

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import Homepage from "./pages/Homepage"
 import Hakkımda from "./pages/Hakkımda"
 import Tarifler from "./pages/Tarifler"
+import TarifDetay from "./pages/TarifDetay"
 
 const AppRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const AppRoutes = () => {
         <Route path='/' element={<Homepage />} />
         <Route path='/hakkımda' element={<Hakkımda />} />
         <Route path='/saglikli-tarifler' element={<Tarifler />} />
+        <Route path='/tarif/:id' element={<TarifDetay />} />
     </Routes>
   )
 }
--- src/pages/Tarifler.jsx
+++ src/pages/Tarifler.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { tarifListesi } from '../data'
 
 const Tarifler = () => {
   return (
-    <div>Tarifler</div>
+    <div className='min-h-screen pt-50 flex justify-center items-center'>
+      <div className='grid grid-cols-1 md:grid-cols-4 gap-x-40 gap-y-9'>
+        {tarifListesi.map(tarif => (
+          <Link to={`/tarif/${tarif.id}`} key={tarif.id}>
+            <div className='bg-white rounded-lg shadow-xl p-4'>
+              <img src={tarif.resim} alt={tarif.isim} className='w-full h-32 object-cover rounded-t-lg' />
+              <div className='p-4'>
+                <h2 className='text-gray-800 text-lg font-bold'>{tarif.isim}</h2>
+                <p className='text-gray-600'>{tarif.kisaAciklama}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
   )
 }
 
